b0e5e9ae27281dc1d149a22cad98dbac3f0a517e
kikkipedia <kristinakerlund@gmail.com>
2021-08-17 15:16:52 +0200

    Navbar component added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Login from './components//Login'
 import Translation from './components/Translation'
 import NotFound from './components/NotFound'
 import AppContainer from './hoc/AppContainer';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-
+      <div>
+        <Navigation/>
         <AppContainer>
           <h4>
           </h4>
